586e50bc9d7e3844952dc57887cc00ebf7563c8b
unknown <ayatahadifar78@gmail.com>
2023-07-28 17:03:56 +0330

    'demo'

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -8,7 +8,7 @@ const Analytics = () => {
             <img className='w-[600px] mr-24' src={Laptop} alt="Laptop" />
         </div> 
         <div className='w-[600px] flex flex-col p-4 justify-center items-center'>
-            <p className='text-[#00df9a] font-bold pb-2'>DATA ANALYTICS DASHBOARD</p> 
+            <p className='text-[#00df9a] font-bold pb-2'>ayat ANALYTICS DASHBOARD</p> 
             <h1 className='font-bold text-3xl pb-4 '>Manage Data Analytics Centrally</h1> 
             <p className='pb-4 w-full  '>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim cumque molestias rem sunt nostrum eligendi soluta consequuntur excepturi cupiditate, dolorum praesentium explicabo eius esse rerum ea aliquam id vel cum!
